ba27cab66e8a91af02af07b3158b4fbf50f29812
Sir-PouRia <psadaty1@gmail.com>
2022-03-02 13:56:53 +0330

    adding get_tree_func but it has a bug

--- nodes/experiments.py
+++ nodes/experiments.py
@@ -98,3 +98,65 @@ def getTree(id: str):
 
 
 
+def getTree_Pouria(id: str, type):
+
+    with driver.session() as session:
+        result = session.run(f"match (n {{id:'{id}'}})-[r*]->(en:{type}) return n,r,en")
+        data = []
+        for each in result:
+            data.append(each)
+
+    x = data
+    g = nx.DiGraph()
+    our_dict = {}
+    graph_dict = {}
+
+    for each in x:
+        g.add_node(each['en'].id)
+        nx.set_node_attributes(g, {each['en'].id: list(each['en'].labels)}, name="label")
+        nx.set_node_attributes(g, {each['en'].id: dict(each['en'])}, name="properties")
+
+    rels = []
+    for eachPath in x:
+        for eachRel in eachPath['r']:
+            reltup = (eachRel.nodes[0].id, eachRel.nodes[1].id)
+            if reltup not in rels:
+                rels.append(reltup)
+
+    if len(x) != 0:
+
+        g.add_node(x[0]['n'].id)
+        nx.set_node_attributes(g, {x[0]['n'].id: list(x[0]['n'].labels)}, name="label")
+        nx.set_node_attributes(g, {x[0]['n'].id: dict(x[0]['n'])}, name="properties")
+
+        g.add_edges_from(rels)
+
+        search_list = list(nx.bfs_tree(g, source=x[0]['n'].id).edges())
+
+        root_node = x[0]['n'].id
+        our_df = pd.DataFrame(search_list)
+
+        # unique levels
+        unique_levels = set(list(our_df[0]))
+
+        level = 0
+        our_dict[level] = [root_node]
+        level = +1
+        our_dict[level] = list(our_df.loc[our_df[0] == root_node][1])
+
+        #our_dict[level + 1] = list(our_df[our_df[0].isin(our_dict[level])][1])
+        # if len(our_dict[level+1]) == 0:
+        #    print("x")
+        while True:
+            our_dict[level + 1] = list(our_df[our_df[0].isin(our_dict[level])][1])
+            level += 1
+            if len(list(our_df[our_df[0].isin(our_dict[level])][1])) == 0:
+                break
+        for each in our_dict:
+            if len(our_dict[each]) != 0:
+                graph_dict[each]=our_dict[each]
+
+    return graph_dict, g
+
+
+
